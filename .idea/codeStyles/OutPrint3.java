e7661268c3f4fbb1fa20e41f9b6160f9c10c4a5e
Taras Didyk <tarasdidyk94@gmail.com>
2019-03-06 13:53:19 +0000

    111

--- .idea/codeStyles/OutPrint3.java
+++ .idea/codeStyles/OutPrint3.java
@@ -35,7 +35,7 @@ public class OutPrint {
         System.out.println("Test");
         System.out.println("Test");
         System.out.println("Test");
-        
+
  System.out.println("Test");
         System.out.println("Test");
         System.out.println("Test");
